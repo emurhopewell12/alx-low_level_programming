33ef9de2f48bff4d11b9ee9fe94ef3d2e2178a31
emurhowho efetobore hopewellsclear <emurhopewell12@gmail.com>
2022-09-09 04:49:06 -0700

    alx low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,10 +10,7 @@
  */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");		
-		return (0);
-
+printf("\"Programming is like building a multilingual puzzle");		
+return (0);
 }
