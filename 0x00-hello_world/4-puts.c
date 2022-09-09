4fd32a02929a093bd72386b95fa53df55878fa8a
emurhowho efetobore hopewellsclear <emurhopewell12@gmail.com>
2022-09-09 05:07:16 -0700

    alx low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-
-
-
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * * main - prints the phrase "Programming is like building a multilingual puzzle"
  *
- * Return: Always 0 (Success)
+ * * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 printf("\"Programming is like building a multilingual puzzle");		
