74fd7ffe47ba9d156cbd947a47ea01f932d7d979
emurhowho efetobore hopewells <emurhopewell12@gmail.com>
2022-09-15 14:31:23 -0700

    My task 3

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+int main(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void printrint_number(int n);
+#endif
