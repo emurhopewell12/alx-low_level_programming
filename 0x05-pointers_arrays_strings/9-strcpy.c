58b600020d01d71c441bd4e595b613b39d56ee9f
emurhowho efetobore hopewells <emurhopewell12@gmail.com>
2022-09-19 18:44:06 -0700

    My task 4

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to
+ * by the src
+ * @dest: A bufferto copy the string
+ * @src: the source string to copy
+ *  Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+{
+		dest[index] = src[index];
+		index++;
+}
+	dest[index] = '\0';
+return (dest);
+}
