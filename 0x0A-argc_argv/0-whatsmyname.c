4bf698e93f9bf384bb47f4a2ac11b09f46c81570
emurhowho efetobore hopewells <emurhopewell12>
2022-10-04 08:30:17 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- function that prints the name of the program.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+		printf("%s\n", argv[0]);
+return (0)
+}
